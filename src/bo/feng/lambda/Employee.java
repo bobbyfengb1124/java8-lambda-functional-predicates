68b981dfada5ed476397da8bba932b53f1c48ac6
Feng Bo <bobbyfeng@cse.com.sg>
2017-12-17 11:44:44 +0800

    funcional interfaces and predicates

---
+++ src/bo/feng/lambda/Employee.java
@@ -0,0 +1,50 @@
+/**
+ * author: Feng Bo
+ *
+ * date: Dec 17, 2017
+ */
+package bo.feng.lambda;
+
+public class Employee {
+	private String name;
+	private int age;
+
+	public Employee(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
+
+
+
